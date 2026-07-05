9c38cec9f5972b95a2906b32f4bdd3feae847a91
Zainab Noor <zn6264352@gmail.com>
2026-07-05 15:58:23 +0500

    Add files via upload

---
+++ Project_2.py
@@ -0,0 +1,94 @@
+# Calculator that save, show, and clear history
+
+# file to save history
+
+file = 'history.txt'
+
+# function to show history
+
+def show_history():
+    file_open = open(file , 'r')
+    lines = file_open.readlines()
+
+    if (len(lines)==0):
+        print('No history found !')
+    else:
+        for line in lines:
+            print(line.strip())
+
+    file_open.close()
+
+# file to clear history
+
+def clear_history():
+    clear = open(file , 'w')
+    clear.close()
+
+    print('History cleared !')
+
+# file to save history
+
+def save_history(equation , output):
+    open_file = open(file , 'a')
+    open_file.write(equation + '=' + str(output) + '\n')
+
+    open_file.close()
+
+# file for calculation
+
+import re
+
+def calculation(equation):
+    equation = equation.replace(' ' , '')
+    splitting = re.split('([+\-*/])' , equation)
+
+    num_1 = float(splitting[0])
+    operator = splitting[1]
+    num_2 = float(splitting[2])
+
+    if (operator == '+'):
+        output = num_1 + num_2
+    elif (operator == '-'):
+        output = num_1 - num_2
+    elif (operator == '*'):
+        output = num_1 * num_2
+    elif (operator == '/'):
+        if num_2 == 0:
+            print('Error! \nDivision by zero not possible')
+            return
+        else:
+            output = num_1 / num_2
+    else:
+        print('Invalid operator entered. Clculator only supports +,-,*,and /')
+        return
+    
+    print(output)
+    return output
+
+def main():
+    print("======Simple Calculator======")
+
+    inpi = ''
+    output = None
+
+    while True:
+        inp = input('Enter command (save,history,clear,exit,calculate): ').lower()
+        
+        if (inp == 'exit'):
+            print("Calculator closed.")
+            break
+        elif (inp == 'calculate'):
+            inpi = input('Enter equation: ')
+            output = calculation(inpi)
+        elif (inp == 'save'):
+            if output is None:
+                print("No calculation to save!")
+            else:
+                save_history(inpi, output)
+        elif (inp == 'history'):
+            show_history()
+        elif (inp == 'clear'):
+            clear_history()
+
+
+main()
